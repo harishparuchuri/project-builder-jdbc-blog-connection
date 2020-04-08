1e47312d212d70ba3e805a1beca3393d894b0f4f
harishparuchuri <harishparuchuri007@gmail.com>
2020-04-08 10:49:33 +0530

    connection

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,4 +1,38 @@
 package utility;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
+public class ConnectionManager {
+	
+	public static Connection getConnection() throws Exception {
+		
 
+		Properties p=loadPropertiesFile();
+		{
+				
+				String driver = p.getProperty("driver");
+				String url= p.getProperty("url");
+				String user=p.getProperty("username");
+				String password=p.getProperty("password");
+				 Class.forName(driver);
+				 Connection con;
+				 con=DriverManager.getConnection(url,user,password);
+				 
+				 return con;
+				 
+
+			}
+		
+	}
+	
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
+}
